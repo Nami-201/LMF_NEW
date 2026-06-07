1292d8620ddaa8eb785744ee68480ff154e155fd
Nami-201 <akvasu13@gmail.com>
2026-06-07 23:24:02 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,7 @@ export default defineConfig({
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
-    server: {{ entry: "server" },
-    allowedHosts: ["lmf-new.onrender.com"]}
+    server: { allowedHosts: ["lmf-new.onrender.com"]}
   },
   nitro: {
     preset: "node-server",
